b4449bb9a684c86d649e53492b3cccd3be42379c
Nafis-Sahriar <nafis.apid@gmail.com>
2026-05-20 09:23:11 +0600

    JWT Verified Delete Request

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ async function run() {
       res.json(result);
     });
 
-    app.delete('/deleteRequest/:id', async(req,res) => {
+    app.delete('/deleteRequest/:id',jwtVerifyToken, async(req,res) => {
 
       const id = req.params.id;
 
